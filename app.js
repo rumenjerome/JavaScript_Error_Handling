d96e0c740dabd45e6c45cf42ba194a2fefa36adf
Rumen Jerome <rumen3435@gmail.com>
2023-05-31 09:41:09 -0700

    Completed exercise

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
-// TODO: Enable strict mode
 
-// TODO: Fix the following parameter list
-function parseToJSON(data, data) {
-  // TODO: Add a try/catch block to
-  // attempt to convert 'data' to JSON
+"use strict";
+
+function parseToJSON(data) {
+  try {
   return JSON.parse(data); 
-  
-  // TODO: if an exception is raised
-  // print the error to the console
-  // and return null
+  }
+catch (error) {
+  console.error(error.message)
+return null
+}
 }
 
 let failData = "<h1>Hello World!</h1>"
